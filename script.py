0de506974d997ba324c67ae8b6d4d6c8d32390b4
Sebastián García <Seb@MacBook-Air-de-Sebastian-2.local>
2019-05-28 20:15:50 -0500

    todo sin pdfs

--- script.py
+++ script.py
@@ -1,43 +1,10 @@
 import pandas as pd
 import subprocess, platform
-# from fpdf import FPDF
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 
- 
-
-# print("HOLA")
-
-# bd = pandas.read_csv('bd.csv')
-# print(bd)
-
-# my_dict = { 'nom' : ["Jaime Rodriguez Villada", "Marcela Garcia Rodriguez", "Ruben Dario Pardo Ramos", "Fredy Ortiz Ortiz"],
-#                    'fech' : ["23/03/19","24/03/19", "26/03/19", "26/03/19"],
-#                    'cc': [123456, 908765, 324578, 105925],
-#                    'edad': [28,22,24,25],
-#                    'sex': ['M','F','M','M']
-#                    }
-
-# my_dict['nom'].append("Sebastian Garcia")
-# my_dict['fech'].append("10/05/19")
-# my_dict['cc'].append(123812)
-# my_dict['edad'].append(23)
-# my_dict['sex'].append('M')
-
-# f= open("bd.csv","w+")crear archivo
-# f=open("bd.csv", "a+")
-# f.write("Sebastian Garcia,10/05/19,123456,23,M\n")
-# print(f)
-# f.close() 
-
-# df = pd.DataFrame(my_dict,index = ['Nombre', 'Fecha de Ingreso', 'C.C.', 'Edad', 'Sexo'])
-# df = pd.DataFrame(my_dict)
-# df.columns = ['Nombre', 'Fecha de Ingreso', 'C.C.', 'Edad', 'Sexo']
-# print(df.to_string(index = False))
-
 class Programa:  
   def __init__(self):
-    self.bd = pd.read_csv('bd.csv')
     self.menu()
 
   def menu(self):
@@ -50,7 +17,6 @@ class Programa:
       print ("[3] Generar Informe Usuario")
       print ("[0] Salir")
       op = int(input("\t\tOpcion => "))
-      # op = 3
       if(op == 1):
         self.ingresarUsuario()
       elif(op == 2):
@@ -85,39 +51,23 @@ class Programa:
   def verUsuarios(self):
     self.limpiarConsola()
     print ("\t\t\t\t\t\tLISTA DE USUARIOS:")
-    print(self.bd.to_string(index = False))
+    bd = pd.read_csv('bd.csv')
+    print(bd.to_string(index = False))
     pause = input("\nPresione cualquier tecla para continuar")
-  # def buscarEnDataFrame(self, dato):
-  #   i = 0
-  #   print(len(self.bd['CC']['0'].index))
-  #   # while i<self.db.shape[]
+
   def generarPDF(self):
     self.limpiarConsola()
-    cedulaBusqueda = input("Por favor digite la cedula del usuario al cual desea generar reporte => ")
-    # for e in self.bd['CC']:
-      # if
-    # print(self.bd['CC'])
-    print(len(self.bd))
+    cedulaBusqueda = int(input("Por favor digite la cedula del usuario al cual desea generar reporte => "))
+    bd = pd.read_csv('bd.csv')
     i = 0
     index = 0
-    while i<len(self.bd):
-      if self.bd['CC'][i] == cedulaBusqueda:
+    while i<len(bd):
+      if bd['CC'][i] == int(cedulaBusqueda):
         index = i
       i += 1
 
-    # print(self.bd['CC'][index],self.bd['Nombre'][index])
-    # print(self.buscarEnDataFrame(int(cedulaBusqueda)))
-    nombreArchivo = "Informe"+self.bd['Nombre'][index]+".pdf"
-    # pdf = FPDF()
-    # pdf.add_page()
-    # pdf.set_font("Arial", size=12)
-    # pdf.cell(200, 10, txt="Reporte Clinico", ln=1, align="C")
-    # strUno = "Nombre:"
-    # strDos = "Fecha de Ingreso: 25/05/2019\t\t Edad: 23\t\t Sexo: M"
-    # pdf.cell(200, 10, txt="Nombre: ", ln=1, align="L")
-    # pdf.cell(200, 10, txt="Cedula :", ln=1, align="L")
-    # pdf.output("simple_demo.pdf")
-    # print(letter)
+    nombreArchivo = "Informe"+bd['Nombre'][index]+".pdf"
+    print(nombreArchivo)
     c = canvas.Canvas(nombreArchivo, pagesize=letter)
     medidaX, medidaY = letter
     #medidas (612.0, 792.0)
@@ -125,29 +75,27 @@ class Programa:
     c.drawString(medidaX/2-125, 720, "Laboratorio Clinico Sanitas S.A.")
     c.setFont("Helvetica", 12)
     c.drawString(65,650, "Nombre:")
-    nomStr = str(self.bd['Nombre'][index])
+    nomStr = str(bd['Nombre'][index])
     c.drawString(120,650, nomStr)
     c.drawString(360,650, "Documento:")
-    cedStr = str(self.bd['CC'][index])
+    cedStr = str(bd['CC'][index])
     c.drawString(435,650, cedStr)
     c.drawString(65,630, "Fecha de Ingreso:")
-    fecStr = str(self.bd['Fecha de Ingreso'][index])
+    fecStr = str(bd['Fecha de Ingreso'][index])
     c.drawString(170,630, fecStr)
     c.drawString(300,630, "Edad:")
-    edadStr = str(self.bd['Edad'][index])    
+    edadStr = str(bd['Edad'][index])    
     c.drawString(335,630, edadStr)
     c.drawString(390,630, "Sexo:")
-    sexStr = str(self.bd['Sexo'][index])
+    sexStr = str(bd['Sexo'][index])
     c.drawString(425,630, sexStr)
-
-
     c.save()
     pause = input("\nPresione cualquier tecla para continuar")
 
   def limpiarConsola(self):
     if platform.system()=="Windows":
-      subprocess.Popen("cls", shell=True).communicate() #I like to use this instead of subprocess.call since for multi-word commands you can just type it out, granted this is just cls and subprocess.call should work fine 
-    else: #Linux and Mac
+      subprocess.Popen("cls", shell=True).communicate() 
+    else: 
       print("\033c", end="")
 
 #ejecucion del programa
